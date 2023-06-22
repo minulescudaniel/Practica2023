ca784b142f67f33577342a68af7b67d56043821b
Daniel Minulescu <82037989+minulescudaniel@users.noreply.github.com>
2023-06-22 13:31:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,33 +14,33 @@ r = requests.get(
     f"https://www.google.com/search?q={query}&tbm=shop", headers=headers, cookies=cookies
 )
 
-# Send a GET request to Google Shopping search results page
+
 url = f"https://www.google.com/search?q={query}&tbm=shop"
 
-# Parse the HTML content using BeautifulSoup
+
 soup = BeautifulSoup(r.content, "html.parser")
 
-# Find the product containers on the page
+
 product_containers = soup.find_all("div", class_="sh-dgr__content")
 
-# Prepare the CSV file for writing
+
 csv_filename = "price_history.csv"
 csv_header = ["Nume", "Pret", "Stare", "Magazin", "URL", "Timestamp"]
 
-# Create a dictionary to store the previously recorded prices
+
 previous_prices = {}
 
 try:
-    # Read the previously recorded prices from the CSV file
+
     with open(csv_filename, "r", newline="", encoding="utf-8") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             previous_prices[row["Nume"]] = row["Pret"]
 except FileNotFoundError:
-    # If the CSV file is not found, initialize an empty dictionary
+
     previous_prices = {}
 
-# Prepare the CSV file for writing
+
 with open(csv_filename, "a", newline="", encoding="utf-8") as csvfile:
     writer = csv.writer(csvfile)
     if csvfile.tell() == 0:
@@ -48,18 +48,18 @@ with open(csv_filename, "a", newline="", encoding="utf-8") as csvfile:
 
     # Extract product information from each container
     for container in product_containers:
-        # Extract the title
+
         title_element = container.find("div", class_="EI11Pd")
         title = title_element.text.strip() if title_element else ""
 
-        # Extract the price
+
         price_element = container.find("span", class_="a8Pemb OFFNJ")
         price = price_element.text.strip() if price_element else ""
 
         second_hand_element = container.find("span", class_="tD1ls")
         second_hand = second_hand_element.text.strip() if second_hand_element else ""
 
-        # Extract the store
+
         store_element = container.find("div", class_="aULzUe IuHnof")
         store = store_element.text.strip() if store_element else ""
 
@@ -67,10 +67,10 @@ with open(csv_filename, "a", newline="", encoding="utf-8") as csvfile:
         url = url_element["href"] if url_element else ""
         url = "https://www.google.com" + url
 
-        # Get the current timestamp
+
         timestamp = datetime.datetime.now()
 
-        # Print or process the product information as desired
+
         print("Nume:", title)
         print("Pret:", price)
         if second_hand == "":
@@ -80,7 +80,7 @@ with open(csv_filename, "a", newline="", encoding="utf-8") as csvfile:
         print("URL:", url)
         print("---------------------")
 
-        # Check if the price has changed for the current product
+
         if title in previous_prices:
             previous_price = previous_prices[title]
             if previous_price != price:
@@ -90,7 +90,7 @@ with open(csv_filename, "a", newline="", encoding="utf-8") as csvfile:
                     print("Current price:", price)
                     print("---------------------")
 
-        # Write the product information to the CSV file
+
         row = [title, price, second_hand, store, url, timestamp]
         writer.writerow(row)
 
